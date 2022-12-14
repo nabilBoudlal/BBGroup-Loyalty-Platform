e054cb99aceb987220b54efd9825f834982acc1b
Nabil <nabil.boudlal@studenti.unicam.it>
2022-12-14 11:25:44 +0100

    Initial commit

---
+++ src/test/java/it/unicam/cs/ids/BBGroup/LoyaltyPlatform/BbGroupLoyaltyPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.ids.BBGroup.LoyaltyPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbGroupLoyaltyPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
