f5b588405ca5c6ea94d4f08b82467d9e5d4fab54
Nabil <nabil.boudlal@studenti.unicam.it>
2023-01-02 15:57:11 +0100

    Ultima versione funzionante

--- src/main/java/it/unicam/cs/ids/BBGroup/LoyaltyPlatform/controller/SimpleRuleApplierController.java
+++ src/main/java/it/unicam/cs/ids/BBGroup/LoyaltyPlatform/controller/SimpleRuleApplierController.java
@@ -25,6 +25,14 @@ public class SimpleRuleApplierController {
     @Autowired
     private TransactionManager transactionManager;
 
+    /**
+     * It takes a cardId and a transactionId, gets the card and transaction, adds the transaction to the card, validates
+     * the transaction, and then applies the rules of the transaction's loyalty program to the card
+     *
+     * @param cardId The id of the card you want to apply the rule to
+     * @param transactionId The id of the transaction to be applied to the card
+     * @return The card with the updated points
+     */
     @PostMapping("/applyRule/{cardId}/{transactionId}")
     public FidelityCard applyRuleToCard(@PathVariable Long cardId,@PathVariable Long transactionId) throws EntityNotFoundException {
        cardManager.getInstance(cardId).addTransaction(transactionManager.getInstance(transactionId));
